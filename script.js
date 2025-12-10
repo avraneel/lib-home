8c95c88ba806a69071e2758bce5dfcd6e358270a
Avraneel Pal <avraneelpal22@gmail.com>
2025-12-10 21:54:31 +0530

    Configure submit and close buttons

--- script.js
+++ script.js
@@ -4,14 +4,22 @@ const myLibrary = [];
 
 const button = document.querySelector(".add");
 const addBookModal = document.querySelector(".add-book-modal");
-
-button.addEventListener("click", displayForm);
+const bookForm = document.querySelector(".book-form");
+const closeButton = document.querySelector(".close-button");
 
 function displayForm() {
   console.log("You clicked me!");
   addBookModal.showModal();
 }
 
+
+button.addEventListener("click", displayForm);
+bookForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+closeButton.addEventListener("click", () => {
+  addBookModal.close();
+})
 
 function Book(title, author) {
   this.title = title;
